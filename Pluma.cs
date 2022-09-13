9deb70fe6678e368196068b7fe3e61404c446a2e
Jero <jeroblack444gamer@gmail.com>
2022-09-13 19:20:52 -0300

    primer subida

---
+++ Pluma.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lapicera
+{
+    class Pluma
+    {
+        private string marca;
+        private Tinta tinta;
+        private int cantidad;
+
+        public Pluma()
+        {
+            this.marca = "sin marca";
+            this.tinta = null;
+            this.cantidad = 1;
+        }
+
+        public Pluma(string marca) : this()
+        {
+            this.marca = marca;
+        }
+
+        public Pluma(string marca, Tinta tinta) : this(marca)
+        {
+            this.tinta = tinta;
+        }
+
+        public Pluma(string marca, Tinta tinta, int cantidad) : this(marca, tinta)
+        {
+            this.cantidad = cantidad;
+        }
+
+        private string Mostrar()
+        {
+            return $"marca {this.marca}, cantidad {this.cantidad}, tipo {(string)this.tinta}";
+        }
+
+        public static bool operator == (Pluma pluma, Tinta tinta)
+        {
+            bool esIgual = false;
+            if (pluma.tinta == tinta)
+            {
+                esIgual = true;
+            }
+            return esIgual;
+        }
+
+        public static bool operator !=(Pluma pluma, Tinta tinta)
+        {
+            bool esDistinto = false;
+            if (!(pluma.tinta == tinta))
+            {
+                esDistinto = true;
+            }
+            return esDistinto;
+        }
+
+        public static Pluma operator +(Pluma pluma, Tinta tinta)
+        {
+            if(pluma == tinta)
+            {
+                pluma.cantidad++;
+            }
+            return pluma;
+        }
+
+        public static Pluma operator -(Pluma pluma, Tinta tinta)
+        {
+            if (pluma == tinta)
+            {
+                pluma.cantidad--;
+            }
+            return pluma;
+        }
+
+        public static implicit operator string(Pluma pluma)
+        {
+            return pluma.Mostrar();
+        }
+    }
+}
---
+++ Tinta.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Lapicera
+{
+    public class Tinta
+    {
+        private ConsoleColor color;
+        private ETipoTinta tipo;
+
+        // constructores //
+        public Tinta()
+        {
+            this.color = ConsoleColor.Black;
+            this.tipo = ETipoTinta.Comun;
+        }
+
+        public Tinta(ETipoTinta tipo):this()
+        {
+            this.tipo = tipo;
+        }
+
+        public Tinta(ETipoTinta tipo, ConsoleColor color):this(tipo)
+        {
+            this.color = color;
+        }
+
+        //----------------//
+
+        private string Mostrar()
+        {
+            return (string)this;
+        }
+
+        public string Mostrar(Tinta tinta)
+        {
+            return tinta.Mostrar();
+        }
+
+
+        // sobre carga operadores //
+
+        public static bool operator == (Tinta t1, Tinta t2)
+        {
+            bool esIgual = false;
+            if((t1.tipo == t2.tipo) && (t1.color == t2.color))
+            {
+                esIgual = true;
+            }
+            return esIgual; 
+        }
+
+        public static bool operator !=(Tinta t1, Tinta t2)
+        {
+            bool esDistinto = false;
+            if (!((t1.tipo == t2.tipo) && (t1.color == t2.color)))
+            {
+                esDistinto = true;
+            }
+            return esDistinto;
+        }
+
+        public static explicit operator string(Tinta tinta)
+        {
+            return $"Tipo{tinta.tipo}, Color{tinta.color}";
+        }
+    }
+}
